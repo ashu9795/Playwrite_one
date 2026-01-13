f5120bf7e6a2637d5bac4e64401312f54494bd36
Ashutosh9795 <ashutoshg067@gmail.com>
2026-01-13 07:48:26 +0530

    Working code

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,12 +1,14 @@
 import { test, expect } from '@playwright/test';
 
-test("PortFolio Test", async ({ page }) => {
+test ("PortFolio Test", async ({ page }) => {
 
   await page.goto('https://ashu9795.netlify.app/');
 
   await page.getByRole('img').first().click();
+  await page.locator('//*[@id="username"]').fill('ashu');
+   await page.getByRole('textbox', { name: 'Password' }).fill('Apple@123');
 
-
+await page.getByRole('button', { name: 'Login' }).click();
 
 
   await page.waitForTimeout(2000);
